ccbb5ce4d72d4a75274d790f3e14c150ca7c29fc
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2023-04-22 07:57:25 +1000

    feat: takes into account if node is the 1st one when setting branch height

--- ng-folder-structure-maker/src/app/components/node-branch/node-branch.component.ts
+++ ng-folder-structure-maker/src/app/components/node-branch/node-branch.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-node-branch',
   templateUrl: './node-branch.component.html',
-  styleUrls: ['./node-branch.component.scss']
+  styleUrls: ['./node-branch.component.scss'],
 })
 export class NodeBranchComponent {
-
+  @Input() isFirstBranch: boolean | undefined;
 }
